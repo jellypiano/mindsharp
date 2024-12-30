195dcc49b6ac256bb899e85e927ed32a6fbf92ba
Lokesh <165713232+jellypiano@users.noreply.github.com>
2024-12-29 19:17:31 -0600

    Delete lib directory

--- lib/quizicist/__init__.py
+++
@@ -1,2 +0,0 @@
-from .completion import complete as generate_quiz
-from .prompt import PromptType
--- lib/quizicist/errors.py
+++
@@ -1,3 +0,0 @@
-# custom exception class for API errors
-class QuizicistError(Exception):
-    pass
